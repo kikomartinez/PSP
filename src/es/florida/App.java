f197898f34a485fcc6f7de101b4c87976829dd2c
quicomartinez11 <elok11@gmail.com>
2020-10-05 09:29:50 +0200

    AE1-2 Creo el método writeNamesFromArray y writeNamesFromList

--- src/es/florida/App.java
+++ src/es/florida/App.java
@@ -1,8 +1,28 @@
 package es.florida;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
 
     public void sayHello(){
         System.out.println("Hello World");
     }
+
+    public void writeNamesFromArray(){
+        String[] names = {"Juan", "Pepe", "Sandra", "Erika", "David", "Kiko"};
+        for (String name: names){
+            System.out.println(name);
+        }
+    }
+
+    public void writeNamesFromList(){
+        List<String> names = Arrays.asList("Juan", "Pepe", "Sandra", "Erika", "David", "Kiko");
+        for (String name: names) {
+            System.out.println(name);
+        }
+    }
+
 }
--- src/es/florida/Main.java
+++ src/es/florida/Main.java
@@ -1,11 +1,19 @@
 package es.florida;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         App app = new App();
         //AE1-1
-        app.sayHello();
+        //app.sayHello();
+
+        //AE1-2
+        app.writeNamesFromArray();
+        app.writeNamesFromList();
     }
 }
